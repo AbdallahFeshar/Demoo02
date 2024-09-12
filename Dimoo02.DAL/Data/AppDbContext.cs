e90e844611c14a9c899d238dc4217c23d0c3ab10
عبد الله محمد عطيه عبد الرحمن فيشار stu <322223631@sha.edu.eg>
2024-09-12 10:43:51 +0300

    End Video From Week

---
+++ Dimoo02.DAL/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Dimoo02.DAL.Data.Configuration;
+using Dimoo02.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dimoo02.DAL.Data
+{
+    internal class AppDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=. ; Database=Demooo ; Trusted_Connection=true ; MultipleActiveResultSets = True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration<Department>(new DepartmentConfiguration());
+            modelBuilder.ApplyConfigurationsFromAssembly(System.Reflection.Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet <Department> Departments { get; set; }
+    }
+}
